d3d05deeb8f8d6b7cfd919a2e938e7ea72bb16d9
Jason Mui <Jasonmui22@gmail.com>
2021-01-21 14:22:47 -0500

    Deployment changes

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://shrouded-sierra-66546.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
